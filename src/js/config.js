d04efcea7906222cc9652289437a1846a892761b
Luis0lo <58235960+Luis0lo@users.noreply.github.com>
2021-03-22 11:51:23 +0000

    helpers and config files

---
+++ src/js/config.js
@@ -0,0 +1,4 @@
+//all the variables that should be constant and reuse cross the project
+
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const TIMEOUT_SEC = 10;
--- src/js/model.js
+++ src/js/model.js
@@ -1,4 +1,6 @@
 import { async } from 'regenerator-runtime';
+import { API_URL } from './config.js';
+import { getJSON } from './helpers.js';
 
 //export to be used in the controller
 export const state = {
@@ -8,15 +10,7 @@ export const state = {
 export const loadRecipe = async function (id) {
   //Ajax request to the Api
   try {
-    const res = await fetch(
-      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
-    );
-    //convert the result to JSON
-    const data = await res.json();
-
-    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
-
-    // console.log(res, data);
+    const data = await getJSON(`${API_URL}/${id}`);
 
     const { recipe } = data.data;
     state.recipe = {
@@ -32,6 +26,7 @@ export const loadRecipe = async function (id) {
 
     console.log(state.recipe);
   } catch (err) {
-    alert(err);
+    //temp error handling
+    console.error(`${err} ⚡⚡⚡`);
   }
 };

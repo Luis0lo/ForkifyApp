8cbb75851b16ce57c933427ce6242b467ea3fcdc
Luis0lo <58235960+Luis0lo@users.noreply.github.com>
2021-03-24 07:42:54 +0000

    implementing search results part2

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,10 +1,15 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
 
 // make old browser be supported by our app
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
+//this module comes from parcel
+if (module.hot) {
+  module.hot.accept();
+}
 
 const timeout = function (s) {
   return new Promise(function (_, reject) {
@@ -37,13 +42,14 @@ const controlRecipes = async function () {
 
 const controlSearchResults = async function () {
   try {
+    resultsView.renderSpinner();
     //1) get search query
     const query = searchView.getQuery();
     if (!query) return;
     //2) load search results
     await model.loadSearchResults(query);
     //3) render results
-    console.log(model.state.search.results);
+    resultsView.render(model.state.search.results);
   } catch (err) {
     console.log(err);
   }
